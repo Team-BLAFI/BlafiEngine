f1d0f783e4988046e3d74a9f728a9f943cfd90cc
Angel Pena <theangel3535@gmail.com>
2023-09-26 00:12:22 -0400

    fix bugs in mouseListener implementation

--- src/util/io/ML.java
+++ src/util/io/ML.java
@@ -1,18 +1,26 @@
 package util.io;
 
 import util.Rect;
+
+import java.awt.dnd.DragGestureEvent;
+import java.awt.dnd.DragGestureListener;
+import java.awt.dnd.MouseDragGestureRecognizer;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class ML extends MouseAdapter implements MouseMotionListener {
 
 
     private static ML mouseListener = new ML();
-    private boolean mousePressed = false;
+    private Dictionary<Integer,Boolean> buttonPressedDictionary = new Hashtable<>();
+
+    private boolean mouseDragging = false;
 
 
-    private double x = 0.0, y = 0.0, previous_x = 0.0, previous_y = 0;
+    private double x = 0.0, y = 0.0;
 
     public static ML getMouseListener(){
         if(ML.mouseListener == null){
@@ -24,27 +32,33 @@ public class ML extends MouseAdapter implements MouseMotionListener {
 
     @Override
     public void mousePressed(MouseEvent e){
-        mousePressed = true;
+        buttonPressedDictionary.put(e.getButton(),true);
     }
 
     @Override
     public void mouseReleased(MouseEvent e){
-        mousePressed = false;
+        buttonPressedDictionary.put(e.getButton(),false);
+        mouseDragging = false;
+
     }
 
     @Override
     public void mouseMoved(MouseEvent e){
-
-        previous_x = x;
-        previous_y = y;
         x = e.getX();
         y = e.getY();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e){
+        mouseDragging = true;
+        mouseMoved(e);
+        e.consume();
     }
 
     public boolean isMouseInsideRect(Rect rect){
 
 
-        boolean  leftEdge = mouseListener.x >= rect.x;
+        boolean  leftEdge  = mouseListener.x >= rect.x;
         boolean  RightEdge = mouseListener.x <= rect.x+ rect.w;
         boolean  UpperEdge = mouseListener.y >= rect.y;
         boolean  LowerEdge = mouseListener.y <= rect.y+rect.h;
@@ -60,16 +74,13 @@ public class ML extends MouseAdapter implements MouseMotionListener {
         return y;
     }
 
-    public double getdx(){
-        return x - previous_x;
-    }
 
-    public double getdy(){
-        return y - previous_y;
+    public boolean isPressed(int buttonCode) {
+        try{
+            return buttonPressedDictionary.get(buttonCode);
+        }catch (Exception e){
+            return  false;
+        }
     }
-
-
-    public boolean isPressed() {
-        return mousePressed;
-    }
+    public boolean isMouseDragging(){return mouseDragging;}
 }
