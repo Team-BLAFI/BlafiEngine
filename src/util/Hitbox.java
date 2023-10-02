4f8a54bd2c71e78f019bf811f067c9ea0c31c8fb
Boschio <boschio91@gmail.com>
2023-10-02 16:55:09 -0400

    Init commit for collision

---
+++ src/util/Hitbox.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Hitbox extends Rect {
+
+    public Hitbox(int x, int y, int w, int h) {
+        super(x, y, w, h);
+
+    }
+
+}
---
+++ src/util/Hurtbox.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Hurtbox extends Rect {
+
+    public Hurtbox(int x, int y, int w, int h) {
+        super(x, y, w, h);
+
+    }
+
+}
