1bfc1182772edb51f102845a0d6a133a2769f222
Angel Pena <theangel3535@gmail.com>
2023-10-03 05:23:34 -0400

    Add Animator Component

--- src/component/Component.java
+++ src/component/Component.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 public abstract class Component {
 
+    public abstract void init();
     public abstract void update(double deltaTime);
 
     public abstract void draw(Graphics g);
